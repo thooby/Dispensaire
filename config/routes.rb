ae442849a29f50b7463f2b737509dd314162a5ba
Juan Zubillaga (Mac) <juan@zubi.net>
2011-04-25 13:10:07 +0200

    Three errors fixed: initial index search, 'nouveau patient' in show, 1995 in patient date inscription

--- config/routes.rb
+++ config/routes.rb
@@ -80,7 +80,7 @@ Dispen3::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
