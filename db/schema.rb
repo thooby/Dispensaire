6baa6b510abcc6e5f7b8e8c07376d1b1cc003588
zubi <juan@zubi.net>
2012-06-26 02:06:30 +0200

    fixing merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120625220430) do
+ActiveRecord::Schema.define(:version => 20120625140023) do
 
   create_table "communes", :force => true do |t|
     t.string   "nom"
