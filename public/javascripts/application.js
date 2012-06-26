7dc5bfc1a94b617bf0d9c61ea10d81685ebbdd10
Tania <tania@efaber.net>
2012-06-26 21:12:04 +0200

    Fixes javascript errors. French translations

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-#function add_fields(link, association, content) {
-#  var new_id = new Date().getTime();
-#  var regexp = new RegExp("new_" + association, "g")
-#  $(link).up().insert({
-#    before: content.replace(regexp, new_id)
-#  });
-#}
-jQuery(function($) {
+// #function add_fields(link, association, content) {
+// #  var new_id = new Date().getTime();
+// #  var regexp = new RegExp("new_" + association, "g")
+// #  $(link).up().insert({
+// #    before: content.replace(regexp, new_id)
+// #  });
+// #}
+
+$(document).ready(function() {
   // when the #country field changes
   $("#patient_commune_id").change(function() {
     // make a POST call and replace the content
